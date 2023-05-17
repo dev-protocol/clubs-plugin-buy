bb05b9183d4540fb87f4f740261d5b34c0b7609a
Yash Agrawa <yash@devprotocol.xyz>
2023-05-17 13:39:18 +0530

    feat: add constants from clubsx

---
+++ src/constants/products.ts
@@ -0,0 +1,16 @@
+export type Product = Readonly<{
+    id: string
+    name: string
+    description?: string
+    price: number
+    currency: 'DEV' | 'ETH'
+    imageSrc: string
+    imageAlt: string
+    payload: Uint8Array
+    fee?: {
+      percentage: number
+      beneficiary: string
+    }
+  }>
+  
+  export type Products = ReadonlyArray<Product>
